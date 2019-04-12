75593da6a1c3b5cc2954ea9783526dcd42d7233d
odan <d.opitz@outlook.com>
2019-04-13 01:31:45 +0200

    Init

--- public/index.php
+++ public/index.php
@@ -11,5 +11,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $container = require __DIR__ . '/../config/bootstrap.php';
 
 // Dispatch
-$response = $container->get(Router::class)->dispatch($container->get(ServerRequestInterface::class));
-(new SapiEmitter())->emit($response);
+(new SapiEmitter())->emit($container->get(Router::class)->dispatch($container->get(ServerRequestInterface::class)));
